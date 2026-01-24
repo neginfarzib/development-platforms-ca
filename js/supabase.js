d07b93ea427febcb887e2519b19eefd88ee38b3f
neginfarzib <88443139+neginfarzib@users.noreply.github.com>
2026-01-24 23:04:19 +0100

    update supabase.js
    fixing the syntax error

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = 'https://nywbqeglebhykvbxvacp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55d2JxZWdsZWJoeWt2Ynh2YWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyOTk5MTcsImV4cCI6MjA4Mzg3NTkxN30.SQn3PiBmAMuLrKfPFd8z1ZFPOde34y6RMlNz4K9A-lY';
+const supabaseUrl = "https://nywbqeglebhykvbxvacp.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55d2JxZWdsZWJoeWt2Ynh2YWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyOTk5MTcsImV4cCI6MjA4Mzg3NTkxN30.SQn3PiBmAMuLrKfPFd8z1ZFPOde34y6RMlNz4K9A-lY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
